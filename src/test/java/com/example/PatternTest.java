dd4ef389c43a5d1af74517d834370f49158990de
Margarita Kurushina <kurushinam@gmail.com>
2013-07-24 21:10:23 +0700

    Add test

--- src/test/java/com/example/PatternTest.java
+++ src/test/java/com/example/PatternTest.java
@@ -86,6 +86,8 @@ public class PatternTest {
 
         Assert.assertEquals("%\nde%", compiler.compile("%%%nde%%").apply(caller, loggerName, level));
 
+        compiler.compile("%d").apply(caller, loggerName, level);
+
 
         Assert.assertEquals("D", compiler.compile("%1.1p").apply(caller, loggerName, level));
 
