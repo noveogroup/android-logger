2f1e9290d30aa4803bd5ac42a67c79532dc24e75
Pavel Stepanov <stefan.nsk@gmail.com>
2015-03-02 15:08:31 +0600

    add SLF4J test

--- AndroidLoggerSample/src/main/java/com/noveogroup/android/sample/log/MainActivity.java
+++ AndroidLoggerSample/src/main/java/com/noveogroup/android/sample/log/MainActivity.java
@@ -7,7 +7,12 @@ import android.view.View;
 import com.noveogroup.android.log.Log;
 import com.noveogroup.android.sample.log.downloader.Downloader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MainActivity extends Activity {
+
+    private static final Logger SLF_LOGGER = LoggerFactory.getLogger(MainActivity.class);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,12 @@ public class MainActivity extends Activity {
         Log.w("test WARN");
         Log.e("test ERROR");
         Log.a("test ASSERT");
+
+        SLF_LOGGER.trace("test trace : {}", "message 100%!");
+        SLF_LOGGER.debug("test debug : {}", "message 100%!");
+        SLF_LOGGER.info("test info  : {}", "message 100%!");
+        SLF_LOGGER.warn("test warn  : {}", "message 100%!");
+        SLF_LOGGER.error("test error : {}", "message 100%!");
     }
 
     public void onSampleSimple(View view) {
